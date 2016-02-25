a4372d81d874973f3a80acb0bf66c870fd2f1b73
Jichao Ouyang <oyanglulu@gmail.com>
2016-02-25 23:18:15 +0800

    eslint

--- __tests__/chapter1.js
+++ __tests__/chapter1.js
@@ -1,53 +1,53 @@
-import _ from 'mori'
+import _ from 'mori';
 describe('Chapter 1', function() {
     beforeEach(() => {
       spyOn(console, 'log');
-    })
+    });
   describe('#1.1.1 Programming Paradigm', function() {
-    it('print 10 times 命令',function() {
-      for (let i=0;i<10;i++){
-        console.log('命令',i)
+    it('print 10 times 命令', function() {
+      for (let i = 0; i < 10; i++) {
+        console.log('命令', i);
       }
       expect(console.log.calls.length).toBe(10);
-    })
+    });
   });
   describe('#1.1.2 Native functional javascript', function() {
     it('support function as arguments', function() {
-      expect([1,2,3,4].map(function(x){return ++x})).toEqual([2,3,4,5]);
+      expect([1, 2, 3, 4].map((x)=>++x)).toEqual([2, 3, 4, 5]);
     });
 
     describe('mutable', function() {
-      var array = [1,2,3,4];
-      var result = [];
-      for(var i of array){
+      const array = [1, 2, 3, 4];
+      const result = [];
+      for (let i of array) {
         result.push(++i);
       }
       it('mutated the result array', function() {
-        expect(result).toEqual([2,3,4,5]);
-      })
+        expect(result).toEqual([2, 3, 4, 5]);
+      });
     });
   });
 
   describe('#1.1.4 Lexical Binding', function() {
-    var Multipler = function(inc){
+    let Multipler = function(inc){
       this.inc = inc;
     };
 
     describe('self = this', function() {
       Multipler.prototype.multiple = function(numbers) {
-        var self = this;
+        const self = this;
         return numbers.map(function(number){
           return self.inc * number;
         });
       };
       it('works', function() {
-        expect(new Multipler(2).multiple([1,2,3,4])).toEqual([2,4,6,8]);
+        expect(new Multipler(2).multiple([1, 2, 3, 4])).toEqual([2, 4, 6, 8]);
       });
     });
 
     describe('arrow function', function() {
-      Multipler.prototype.multiple = function(numbers){
-        return numbers.map(number => number*this.inc);
+      Multipler.prototype.multiple = function(numbers) {
+        return numbers.map(number => number * this.inc);
       };
       it('works', function() {
         expect(new Multipler(2).multiple([1,2,3,4])).toEqual([2,4,6,8]);
@@ -57,26 +57,26 @@ describe('Chapter 1', function() {
 
   describe('#1.1.6 Desctructure', () => {
     describe('eating oreo', function() {
-      let dipMilk = jest.genMockFunction(),
+      const dipMilk = jest.genMockFunction(),
           lip = jest.genMockFunction().mockReturnValue('wetMiddleAndButton'),
           eat = jest.genMockFn();
 
       describe('the old way', function() {
-        let orea = ["top","middle","bottom"],
+        const orea = ['top', 'middle', 'bottom'],
             top = orea.shift(), middleAndButton=orea,
             wetMiddleAndButton = dipMilk(middleAndButton),
             button = lip(wetMiddleAndButton);
-        it("sucks", function() {
+        it('sucks', function() {
           eat([top, button]);
           expect(eat).toBeCalledWith(['top', 'wetMiddleAndButton'])
         });
       });
 
       describe('the es6 way', function() {
-        let [top, ...middleAndButton] = ["top","middle","bottom"];
-        let wetMiddleAndButton = dipMilk(middleAndButton) ;
-        let button = lip(wetMiddleAndButton);
-        it("is awesome", function() {
+        const [top, ...middleAndButton] = ['top', 'middle', 'bottom'];
+        const wetMiddleAndButton = dipMilk(middleAndButton);
+        const button = lip(wetMiddleAndButton);
+        it('is awesome', function() {
           eat([top, button]);
           expect(eat).toBeCalledWith(['top', 'wetMiddleAndButton']);
         });
@@ -86,23 +86,24 @@ describe('Chapter 1', function() {
 
   describe('#1.2.1 Immutable', () => {
     describe('Array is Mutable', () => {
-      var a = [1,2,3]
-      a.push(4)
+      let a = [1, 2, 3];
+      a.push(4);
       it('a should be mutated', () => {
-        expect(a).toEqual([1,2,3,4])
+        expect(a).toEqual([1, 2, 3, 4]);
       });
     });
     describe('Vector is Immutable', () => {
-      var a = _.vector(1,2,3)
-      expect(_.conj(a, 4).toString()).toEqual('[1 2 3 4]')
+      let a = _.vector(1, 2, 3);
+      expect(_.conj(a, 4).toString()).toEqual('[1 2 3 4]');
     });
   });
 
-  describe('#1.2.4', () => {
-    var a = [1,2,3,4]
-    var b = [4,3,2,1]
-    for(var i=0;i<4;i++)
-      a[i]+=b[i]
-    expect(a).toEqual([5,5,5,5])
+  describe('#1.2.4 loop', () => {
+    let a = [1, 2, 3, 4];
+    let b = [4, 3, 2, 1];
+    for (let i = 0; i < 4; i++) {
+      a[i] += b[i];
+    }
+    expect(a).toEqual([5, 5, 5, 5]);
   });
 });
