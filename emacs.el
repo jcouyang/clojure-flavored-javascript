818a125db93b243224c647df122720b18a1a0ffe
Jichao Ouyang <oyanglulu@gmail.com>
2016-03-01 22:05:55 +0800

    add tufte cls

--- emacs.el
+++ emacs.el
@@ -4,12 +4,30 @@
 (require 'clojure-mode)
 (clojure-font-lock-setup)
 (require 'org)
+(require 'ox-latex)
 (require 'htmlize)
 (setq make-backup-files nil)
-(setq debug-on-error t)
 (setq book-path (expand-file-name "book"))
 (setq org-html-validation-link nil)
 (setq org-confirm-babel-evaluate nil)
+(add-to-list 'org-latex-classes
+               '("tufte" "\\documentclass[11pt,twoside,openright,a5paper]{tufte-book}"
+                  ("\\chapter{%s}" . "\\chapter*{%s}")
+                  ("\\section{%s}" . "\\section*{%s}")
+                  ("\\subsection{%s}" . "\\subsection*{%s}")
+                  ))
+(setq tex-compile-commands '(("xelatex %r")))
+(setq tex-command "xelatex")
+(setq-default TeX-engine 'xelatex)
+
+(setq org-latex-pdf-process
+      '("xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"))
+
+(setq locate-command "mdfind")
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/share/npm/bin:/usr/local/bin:/usr/texbin"))
+(setq exec-path (append exec-path '("/usr/local/bin" "/usr/texbin")))
 (custom-set-variables
   '(org-publish-timestamp-directory
      (convert-standard-filename "public/.org-timestamps/")))
@@ -23,7 +41,7 @@
   "Set publishing projects for Orgweb and Worg."
   (interactive)
   (setq org-publish-project-alist
-    `(("books"
+    `(("html"
        ;; Directory for source files in org format
         :base-directory ,book-path
         :base-extension "org"
@@ -38,13 +56,29 @@
         :headline-levels 5
         ;; :with-sub-superscript nil
         :section-numbers 3
+        :makeindex t
+        :exclude ".*"
+        :include ("index.org" "zh/index.org")
         :html-head-include-default-style nil
+        )
+      ("pdf"
+       ;; Directory for source files in org format
+        :base-directory ,book-path
+        :base-extension "org"
+        :publishing-directory "public/pdf"
+        :publishing-function org-latex-publish-to-pdf
+        :headline-levels 5
+        ;; :with-sub-superscript nil
+        :section-numbers 3
+        :makeindex t
+        :exclude ".*"
+        :include ("index.org" "zh/index.org")
         )
 
        ;; where static files (images, pdfs) are stored
        ("blog-static"
          :base-directory ,book-path
-         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|woff2\\|woff"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|mp3\\|ogg\\|swf\\|woff2\\|woff"
          :publishing-directory "public"
          :recursive t
          :publishing-function org-publish-attachment
